be59e064de3455f5d10940cc83e6e8db1140a255
qudtjs0753 <qudtjs0753@naver.com>
2023-05-18 09:58:27 +0900

    Add: 기본 폴더 생성

---
+++ src/main/java/com/ssafy/trip/OpenDataTourismBackApplication.java
@@ -0,0 +1,11 @@
+package com.ssafy.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDataTourismBackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenDataTourismBackApplication.class, args);
+    }
+}
