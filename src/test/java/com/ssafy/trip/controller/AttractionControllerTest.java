9208ff30e4d892cc8490945a5593ca98cf8b150f
qudtjs0753 <qudtjs0753@naver.com>
2023-05-19 11:28:39 +0900

    Add: test 추가

---
+++ src/test/java/com/ssafy/trip/controller/AttractionControllerTest.java
@@ -0,0 +1,14 @@
+package com.ssafy.trip.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AttractionControllerTest {
+
+    @Test
+    public void a() {
+        System.out.println("hi");
+    }
+
+}
