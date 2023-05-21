b57871bef2326b090682bf9290697aff901ff7b7
qudtjs0753 <qudtjs0753@naver.com>
2023-05-21 17:31:08 +0900

    Add: ControllerAdvice 생성

---
+++ src/main/java/com/ssafy/trip/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.ssafy.trip.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
