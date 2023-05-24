990b5c9c85565aa3368fd3bd5e2ad595492b274c
qudtjs0753 <qudtjs0753@naver.com>
2023-05-24 10:56:09 +0900

    Add: 인터셉터를 사용한 세션 검사 로직

--- src/main/java/com/ssafy/trip/config/WebConfiguration.java
+++ src/main/java/com/ssafy/trip/config/WebConfiguration.java
@@ -1,9 +1,12 @@
 package com.ssafy.trip.config;
 
+import com.ssafy.trip.interceptor.AuthorizationInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,5 +21,16 @@ public class WebConfiguration implements WebMvcConfigurer {
                         HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
                         HttpMethod.PATCH.name())
                 .maxAge(1800);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor())
+                .excludePathPatterns("/board/**/list")
+                .addPathPatterns("/board/**");
+    }
+
+    private HandlerInterceptor authorizationInterceptor() {
+        return new AuthorizationInterceptor();
     }
 }
