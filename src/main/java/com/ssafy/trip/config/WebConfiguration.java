c534a67e3f64fcccd95c9b782c52c8ced0e0d7d1
qudtjs0753 <qudtjs0753@naver.com>
2023-05-24 19:22:53 +0900

    Add: XSS 보안을 막기 위해 jsonEscapeConverter를 사용해 문자 변환

--- src/main/java/com/ssafy/trip/config/WebConfiguration.java
+++ src/main/java/com/ssafy/trip/config/WebConfiguration.java
@@ -1,16 +1,19 @@
 package com.ssafy.trip.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.trip.filter.xss.HTMLCharacterEscapes;
 import com.ssafy.trip.interceptor.AuthorizationInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
@@ -28,6 +31,14 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addInterceptor(authorizationInterceptor())
                 .excludePathPatterns("/board/**/list")
                 .addPathPatterns("/board/**");
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+        // MappingJackson2HttpMessageConverter Default ObjectMapper 설정 및 ObjectMapper Config 설정
+        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
+        objectMapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
+        return new MappingJackson2HttpMessageConverter(objectMapper);
     }
 
     private HandlerInterceptor authorizationInterceptor() {
